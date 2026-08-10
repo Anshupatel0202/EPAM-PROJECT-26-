1b85eef9e57e0a5d31d1c7535595028afb52a3b2
Anshupatel0202 <anshupatel0202@gmail.com>
2026-08-10 22:40:41 +0530

    Create Problem1.java

---
+++ Assignment 2/Problem1.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Problem1 {
+
+    static ArrayList<Integer>[] tree;
+    static int[] key;
+    static int K;
+    static int count = 0;
+
+    static void dfs(int node, int parent, int xorValue) {
+
+        xorValue = xorValue ^ key[node];
+
+        if (xorValue >= K) {
+            count++;
+        }
+
+        for (int next : tree[node]) {
+            if (next != parent) {
+                dfs(next, node, xorValue);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        K = sc.nextInt();
+
+        key = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            key[i] = sc.nextInt();
+        }
+
+        tree = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            tree[u].add(v);
+            tree[v].add(u);
+        }
+
+        dfs(1, 0, 0);
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
